f9902558e85873dc1ddb35831a1ea8fe12206f0b
William Casarin <bill@casarin.me>
2014-03-31 17:46:29 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+var moment = require('moment')
+
+function def(str, d) {
+  var num = parsers.number(str);
+  return num === null? d : num;
+}
+
+function orN(str, d) {
+  return str === ""? d : str;
+}
+
+function or1(str, d) {
+  return orN(str, 1);
+}
+
+var parsers = module.exports = function(csv) {
+  var parsed = {};
+  parsed.itemName = csv.col('item name');
+  parsed.itemType = csv.col('item type');
+  parsed.upc = csv.col('upc');
+  parsed.isrc = csv.col('isrc');
+  parsed.artist = csv.col('artist');
+  parsed.sales = parsers.number(or1(csv.col('quantity')));
+  parsed.orderDate = parsers.orderDate(csv.col('order date'));
+  parsed.referrer = csv.col('referrer url')
+  parsed.buyer = {
+    name: csv.col('buyer name'),
+    phone: csv.col('buyer phone'),
+    note: csv.col('buyer note'),
+    email: csv.col('buyer email')
+  }
+  return parsed;
+};
+
+parsers.orderDate = function(str){
+  return moment(str, 'M/D/YY h:mma').toDate();
+}
+
+parsers.number = require('parse-number').str;
+
+parsers.paypal = function(str) {
+  return def(str, 0);
+}
---
+++ test/bandcamp.js
@@ -0,0 +1,65 @@
+
+var assert = require('better-assert')
+var devnull = require('dev-null')
+var csv = require('csv')
+var debug = require('debug')('parse-bandcamp-csv:test');
+var join = require('path').join;
+var parse = require('..');
+var recordParser = require('csv-record-parser');
+
+var file = process.env.BANDCAMP_TEST_CSV || join(__dirname, "bandcamp.csv")
+
+
+describe('bandcamp csv parser', function(){
+  it('parses date correctly', function(){
+    var date = parse.orderDate("1/15/14 12:10pm")
+    assert(date.getMonth() === 0);
+    assert(date.getDate() === 15);
+    assert(date.getSeconds() === 0);
+    assert(date.getFullYear() === 2014);
+    assert(date.getHours() === 12);
+  });
+
+  it('parses and types are ok', function(done){
+    var parser = recordParser();
+    var first = true;
+
+    csvFile(file, function(row){
+      if (first) {
+        parser.header(row);
+        first = false;
+        return;
+      }
+      parser.row(row);
+      var bandcamp = parse(parser);
+
+      debug("row %j", bandcamp);
+      assert(bandcamp.itemName != null);
+      assert(bandcamp.itemType != null);
+      assert(bandcamp.referrer != null);
+      assert(bandcamp.itemType === 'album' || bandcamp.itemType === 'track');
+      assert(bandcamp.sales != null);
+      assert(bandcamp.sales >= 0);
+      assert(bandcamp.orderDate != null);
+      assert(bandcamp.orderDate.getTime() < new Date().getTime());
+      assert(bandcamp.buyer != null);
+      assert(bandcamp.buyer.name != null);
+      assert(bandcamp.buyer.note != null);
+      assert(bandcamp.buyer.email != null);
+      assert(bandcamp.upc != null);
+      assert(bandcamp.artist != null);
+      assert(bandcamp.isrc != null);
+    }, function(){
+      done();
+    });
+  });
+});
+
+function csvFile(file, rowFn, done) {
+  return csv().from.path(file)
+  .to.stream(devnull())
+  .on('record', rowFn)
+  .on('end', function(){
+    done();
+  })
+}
